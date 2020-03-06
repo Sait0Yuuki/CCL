80f38481c32f55e7e790c3371b76d2cab7765bcf
isanthree <h1486636414@163.com>
2020-03-06 14:09:33 +0800

    加上命令提示

--- Lab1/src/Sudoku/main.cc
+++ Lab1/src/Sudoku/main.cc
@@ -1,6 +1,7 @@
 #include <assert.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h> 
 #include <string.h>
 #include <sys/time.h>
 
@@ -8,50 +9,62 @@
 
 int64_t now()
 {
-  struct timeval tv;
-  gettimeofday(&tv, NULL);
-  return tv.tv_sec * 1000000 + tv.tv_usec;
+	struct timeval tv; //在 _timeval.h 中声明了该结构体，两个成员变量 tv_sec（秒） 和 tv_usec（微秒）
+	gettimeofday(&tv, NULL);
+	printf("main.cc\n");
+	printf("%d s\n", unsigned(tv.tv_sec + tv.tv_usec * 1000000));
+
+	return tv.tv_sec * 1000000 + tv.tv_usec; //此处是不是反了？tv_sec（秒）？tv_usec（微秒）？
 }
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-  init_neighbors();
-
-  FILE* fp = fopen(argv[1], "r");
-  char puzzle[128];
-  int total_solved = 0;
-  int total = 0;
-  bool (*solve)(int) = solve_sudoku_basic;
-  if (argv[2] != NULL)
-    if (argv[2][0] == 'a')
-      solve = solve_sudoku_min_arity;
-    else if (argv[2][0] == 'c')
-      solve = solve_sudoku_min_arity_cache;
-    else if (argv[2][0] == 'd')
-      solve = solve_sudoku_dancing_links;
-  int64_t start = now();
-  while (fgets(puzzle, sizeof puzzle, fp) != NULL) {
-    if (strlen(puzzle) >= N) {
-      ++total;
-      input(puzzle);
-      init_cache();
-      //if (solve_sudoku_min_arity_cache(0)) {
-      //if (solve_sudoku_min_arity(0))
-      //if (solve_sudoku_basic(0)) {
-      if (solve(0)) {
-        ++total_solved;
-        if (!solved())
-          assert(0);
-      }
-      else {
-        printf("No: %s", puzzle);
-      }
+	if (argc != 2)
+    {
+        fprintf(stderr, "Usage: %s <pathname>\n", argv[0]);
+        exit(EXIT_FAILURE);
     }
-  }
-  int64_t end = now();
-  double sec = (end-start)/1000000.0;
-  printf("%f sec %f ms each %d\n", sec, 1000*sec/total, total_solved);
 
-  return 0;
+	init_neighbors();
+
+	FILE *fp = fopen(argv[1], "r");
+	char puzzle[128];
+	int total_solved = 0;
+	int total = 0;
+	bool (*solve)(int) = solve_sudoku_basic;
+	if (argv[2] != NULL)
+		if (argv[2][0] == 'a')
+			solve = solve_sudoku_min_arity;
+		else if (argv[2][0] == 'c')
+			solve = solve_sudoku_min_arity_cache;
+		else if (argv[2][0] == 'd')
+			solve = solve_sudoku_dancing_links;
+	int64_t start = now();
+	while (fgets(puzzle, sizeof puzzle, fp) != NULL)
+	{
+		if (strlen(puzzle) >= N)
+		{
+			++total;
+			input(puzzle);
+			init_cache();
+			//if (solve_sudoku_min_arity_cache(0)) {
+			//if (solve_sudoku_min_arity(0))
+			//if (solve_sudoku_basic(0)) {
+			if (solve(0))
+			{
+				++total_solved;
+				if (!solved())
+					assert(0);
+			}
+			else
+			{
+				printf("No: %s", puzzle);
+			}
+		}
+	}
+	int64_t end = now();
+	double sec = (end - start) / 1000000.0;
+	printf("%f sec %f ms each %d\n", sec, 1000 * sec / total, total_solved);
+
+	return 0;
 }
-
