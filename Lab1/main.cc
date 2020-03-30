8b854b059cb89d2ae7a5544d94281d9dfca13fc0
isanthree <h1486636414@163.com>
2020-03-30 23:25:49 +0800

    order output and add run time

--- Lab1/main.cc
+++ Lab1/main.cc
@@ -10,47 +10,53 @@
 
 int64_t now()
 {
-  struct timeval tv;
-  gettimeofday(&tv, NULL);
-  return tv.tv_sec * 1000000 + tv.tv_usec;
+	struct timeval tv;
+	gettimeofday(&tv, NULL);
+	return tv.tv_sec * 1000000 + tv.tv_usec;
 }
 
-
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-  init_neighbors();
-  FILE* fp;
-  //fp[0] = fopen(argv[1], "r");
-  char puzzle[128];
-  
-  int total_solved = 0;
-  int total = 0;
-  //int64_t start = now();
-  for(int i=1;i<argc;i++)
-  {
-     fp = fopen(argv[i], "rw");
-     while (fgets(puzzle, sizeof puzzle, fp) != NULL) {
-      if (strlen(puzzle) >= N) {
-      input(puzzle,total++);
-      }
-      else {
-        printf("No: %s", puzzle);
-      }
-  }
-  }
-  
-  ThreadPool pool(THREADNUM);
-  it=puzzleSet.begin();
-  while(it!=puzzleSet.end()){
-    pool.enqueue(solveSudoku);
-    usleep(1000); //sleep to avoid dead loop
-  }
-  output();
+	init_neighbors();
+	FILE *fp;
+	//fp[0] = fopen(argv[1], "r");
+	char puzzle[128];
 
-  //exit(1);
-  //int64_t end = now();
-  //double sec = (end-start)/1000000.0;
-  //printf("%f sec %f ms each %d\n", sec, 1000*sec/total, total_solved);
-  return 0;
+	int total_solved = 0;
+	int total = 0;
+	//int64_t start = now();
+	for (int i = 1; i < argc; i++)
+	{
+		fp = fopen(argv[i], "rw");
+		while (fgets(puzzle, sizeof puzzle, fp) != NULL)
+		{
+			if (strlen(puzzle) >= N)
+			{
+				input(puzzle, total++);
+			}
+			else
+			{
+				printf("No: %s", puzzle);
+			}
+		}
+	}
+
+	using namespace std::chrono;
+	auto start = system_clock::now();
+	ThreadPool pool(THREADNUM);
+	it = puzzleSet.begin();
+	while (it != puzzleSet.end())
+	{
+		pool.enqueue(solveSudoku);
+		usleep(1000); //sleep to avoid dead loop
+	}
+	output();
+	duration<double> diff = system_clock::now() - start;
+	std::cout << "Run: " << diff.count() << "s.\n";
+
+	//exit(1);
+	//int64_t end = now();
+	//double sec = (end-start)/1000000.0;
+	//printf("%f sec %f ms each %d\n", sec, 1000*sec/total, total_solved);
+	return 0;
 }
-
--- Lab1/sudoku_basic.cc
+++ Lab1/sudoku_basic.cc
@@ -9,60 +9,61 @@
 #include "sudoku.h"
 using namespace std;
 
-pthread_mutex_t queue_mutex =PTHREAD_MUTEX_INITIALIZER;
-pthread_mutex_t output_mutex =PTHREAD_MUTEX_INITIALIZER;
-__thread int board[N];   //使用__thread变量使每个线程有一份独立的board实体
-//std::queue<std::vector<int>> puzzleSet;   
-std::map<int,std::vector<int>> puzzleSet;
-map<int,std::vector<int>>::iterator it;
-map<int,std::vector<int>>::iterator ite;
+pthread_mutex_t queue_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t output_mutex = PTHREAD_MUTEX_INITIALIZER;
+__thread int board[N]; //使用__thread变量使每个线程有一份独立的board实体
+//std::queue<std::vector<int>> puzzleSet;
+std::map<int, std::vector<int>> puzzleSet;
+map<int, std::vector<int>>::iterator it;
+map<int, std::vector<int>>::iterator ite;
 int (*chess)[COL] = (int (*)[COL])board;
 
 void solveSudoku()
-{     
-  bool (*solve)(int) = solve_sudoku_dancing_links; 
-  pthread_mutex_lock(&queue_mutex);
-  if(it!=puzzleSet.end()) 
-  {
-    std::vector<int> tmp=it->second;
-    std::copy(tmp.begin(),tmp.end(),board);
-    
-    //puzzleSet.pop();
-  }
-  else
-  {
-    return ;
-  }
-  pthread_mutex_unlock(&queue_mutex);
-  if (solve(0))
-  {
-    
-    std::vector<int> tmp(board,board+81);
-    puzzleSet[it->first]=tmp;
-    it++;
-    //output();
-    //if (!solved())
-      //assert(0);
-  }
-  return ;
+{
+	bool (*solve)(int) = solve_sudoku_dancing_links;
+	pthread_mutex_lock(&queue_mutex);
+	if (it != puzzleSet.end())
+	{
+		std::vector<int> tmp = it->second;
+		std::copy(tmp.begin(), tmp.end(), board);
+
+		//puzzleSet.pop();
+	}
+	else
+	{
+		return;
+	}
+	pthread_mutex_unlock(&queue_mutex);
+	if (solve(0))
+	{
+
+		std::vector<int> tmp(board, board + 81);
+		puzzleSet[it->first] = tmp;
+		it++;
+		//output();
+		//if (!solved())
+		//assert(0);
+	}
+	return;
 }
 
 void output()
 {
-  pthread_mutex_lock(&output_mutex);
-  ite=puzzleSet.begin();
-  while(ite!=puzzleSet.end())
-  {
-    cout<<ite->first<<endl;
-    vector<int> tmp=ite->second;
-    for(int i=0;i<81;i++) cout<<tmp[i];
-    cout<<endl;
-    ite++;
-  }
-  pthread_mutex_unlock(&output_mutex);
+	pthread_mutex_lock(&output_mutex);
+	ite = puzzleSet.begin();
+	while (ite != puzzleSet.end())
+	{
+		// cout << ite->first << endl;
+		vector<int> tmp = ite->second;
+		for (int i = 0; i < 81; i++)
+			cout << tmp[i];
+		cout << endl;
+		ite++;
+	}
+	pthread_mutex_unlock(&output_mutex);
 }
 
-void input(const char in[N],int num)
+void input(const char in[N], int num)
 {
 	std::vector<int> tmp;
 	for (int cell = 0; cell < N; ++cell)
@@ -70,6 +71,6 @@ void input(const char in[N],int num)
 		tmp.push_back(in[cell] - '0');
 		assert(0 <= tmp[cell] && tmp[cell] <= NUM);
 	}
-	puzzleSet.insert(pair<int,vector<int>>(num,tmp));
+	puzzleSet.insert(pair<int, vector<int>>(num, tmp));
 	//std::cout<<puzzleSet.size()<<std::endl;
 }
