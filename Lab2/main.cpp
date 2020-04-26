c4368ffdb5fbcb533ca6d07cf9b033a9b538bcfa
isanthree <h1486636414@163.com>
2020-04-26 16:53:49 +0800

    update GET method

--- Lab2/main.cpp
+++ Lab2/main.cpp
@@ -1,140 +1,168 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<sys/socket.h>
-#include<sys/types.h>
-#include<sys/stat.h>
-#include<sys/sendfile.h>
-#include<fcntl.h>
-#include<netinet/in.h>
-#include<arpa/inet.h>
-#include<assert.h>
-#include<unistd.h>
-#include<string.h>
-#include <errno.h>
 #include <iostream>
+#include <string>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <sys/sendfile.h>
+#include <fcntl.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <assert.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>   
+#include "HttpServer.h"
 
-#include "httprequestparser.h"
-#include "request.h"
-using namespace httpparser;
+// const int port = 8887;
+const int port = 8888;
 
-int server_port;
+string file_path="", request_method="";
 
-void handle_request(char text[1024])
+void http_get_method(int connfd)
 {
-    Request request;
-    HttpRequestParser parser;
-    int length=strlen(text);
+    string path_tmp = ".";
+    path_tmp += file_path;
+    cout << "db db db path_tmp:" << path_tmp <<"\n";
 
-    HttpRequestParser::ParseResult res = parser.parse(request, text, text + length);
-
-    if( res == HttpRequestParser::ParsingCompleted )
+    int fd = open(path_tmp.c_str(),O_RDONLY);//消息体
+    if(fd < 0)
     {
-        std::cout<<request.method<<std::endl;
-        std::cout<<request.uri<<std::endl;
-        //method(GET OR POST)
-        //uri(/,/Post_show)
-        //content(e.g. Name=HNU&ID=CS06142)
+        perror("open file error\n");
+    }
+    else
+    {
+        printf("db db db: found file\n");
+    }
+    
+    struct stat f_stat;
+    stat(path_tmp.c_str(), &f_stat);
+    char buf[520]="HTTP/1.1 200 OK\r\n\r\n";//HTTP响应
+    write(connfd, buf, strlen(buf));
+    sendfile(connfd,fd,NULL,4096);//零拷贝发送消息体
+    // int s = send(connfd,buf,strlen(buf),0);//发送响应
+    // printf("send_buf_length = %d\n",s);
+   
+    close(fd);
+    close(connfd);
+}
+
+//功能划分，为封装类做准备
+void http(int connfd)
+{
+	char request[1024];
+    recv(connfd,request,1024,0);
+
+    // 返回请求的数据
+    request[strlen(request)+1]='\0';
+    printf("%s\n",request);
+    printf("successeful!\n");
+    
+    string str_request = "";
+    str_request += request;
+    cout << "db str_request:" << str_request <<"\n";
+    cout << "I am here!!! \n\n";
+
+    int position = 0;   //字符位置指针
+    if((str_request.find("\r\n\r\n"))!=string::npos)
+    {
+        request_method = "";
+        while(str_request[position]!=' ')
+        {
+            request_method += str_request[position];
+            position++;
+        }
+        cout << "db db db request_method:" << request_method << "\n"; //db 
+        position++;
+        cout << "db db db position: " << position << "\n";
+        file_path = "";
+        while(str_request[position]!=' ')
+        {
+            file_path += str_request[position];
+            position++;
+        }
+        cout << "db db db file_path:" << file_path << "\n";
+
+        if(request_method == "GET")
+        {
+            cout  << "db db db request_method: GET GET GET " << request_method << "\n";
+
+            http_get_method(connfd);
+        }
     }
 }
 
-void TCP_connect()
+int main(int argc,char *argv[])
 {
-    int socketfd;
-    int connfd;
-    struct sockaddr_in server_address;
-
-    socketfd = socket(AF_INET,SOCK_STREAM,0);   //建立TCP socket
-    if (socketfd == -1) {
-    perror("Failed to create a new socket");
-    exit(errno);
-    }
-	
-	int reuse = 1;
-    if(setsockopt(socketfd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0)
-    {
-      perror("setsockopt error\n");
-      return -1;
-    }
-
-    bzero(&server_address,sizeof(server_address));    //清零
-    server_address.sin_family = AF_INET; 
-    server_address.sin_addr.s_addr = htons(INADDR_ANY);  //sin_addr存储IP地址，使用in_addr这个数据结构；s_addr按照网络字节顺序存储IP地址
-    server_address.sin_port = htons(server_port);   //端口号
- 
-
-    if (bind(socketfd, (struct sockaddr *) &server_address,
-        sizeof(server_address)) == -1) {
-        perror("Failed to bind on socket");
-        exit(errno);
-    }
-
-    if (listen(socketfd, 1024) == -1) 
-    {
-        perror("Failed to listen on socket");
-        exit(errno);
-    }
-
-    printf("Listening on port %d...\n",server_port);
-
-    while(true)
-    {
-        //等待连接
-        //https://blog.csdn.net/daiyudong2020/article/details/51868813
-        struct sockaddr_in client;
-        socklen_t client_addrlength = sizeof(client);
-        connfd = accept(socketfd, (struct sockaddr*)&client, &client_addrlength);   //返回连接套接字描述符
-        if(connfd<0)    //该描述符不为0
-        {
-            perror("err connfd\n");
-        }
-        else
-        {
-            char request[1024];
-            recv(connfd,request,1024,0);
-            request[strlen(request)+1]='\0';
-
-            handle_request(request);
-
-            printf("successeful!\n");
-            char buf[520]="HTTP/1.1 200 ok\r\nconnection: close\r\n\r\n";//HTTP响应
-            int s = send(connfd,buf,strlen(buf),0);//发送响应
-            //printf("send=%d\n",s);
-            int fd = open("index.html",O_RDONLY);//消息体
-            sendfile(connfd,fd,NULL,2500);//零拷贝发送消息体
-            close(fd);
-            close(connfd);
-        }
-    }
-	close(socketfd);
-
-}
-
-int main(int argc,char **argv)
-{
-    //默认参数s
-    server_port=8888;
-
     if(argc < 0)
     {
         printf("./httpServer --ip ip_address --port port_number [--number-thread thread_number] \n");
         exit(1);
     }
-
-    for(int i=1;i<argc;i++)
+    int socketfd;
+    int connfd;
+    struct sockaddr_in sever_address;
+    bzero(&sever_address,sizeof(sever_address));    //清零
+    sever_address.sin_family = AF_INET; 
+    sever_address.sin_addr.s_addr = inet_addr("127.0.0.1");
+    // sever_address.sin_addr.s_addr = htons(INADDR_ANY);  //sin_addr存储IP地址，使用in_addr这个数据结构；s_addr按照网络字节顺序存储IP地址
+    sever_address.sin_port = htons(port);   //端口号
+ 
+    socketfd = socket(AF_INET,SOCK_STREAM,0);   //建立TCP socket
+    if(socketfd == -1)
     {
-        if(strcmp("--port",argv[i])==0)  // --port
-        {
-            char *server_port_str=argv[++i];
-            if(!server_port_str){
-                fprintf(stderr, "Expected argument after --port\n");
-            }
-            server_port=atoi(server_port_str);
-        }
-        /*else if..*/
+        perror("Failed to create a new socket\n");
+        exit(errno);
+    }
+    //************************************************************
+    const int reuse = 1;
+    if(setsockopt(socketfd, SOL_SOCKET, SO_REUSEADDR, (char *)&reuse, sizeof(reuse)) < 0)
+    {
+        perror("setsockopt error\n");
+        return -1;
+    }
+    //************************************************************
+    
+    //绑定
+    int ret;
+    if(ret=bind(socketfd, (struct sockaddr*)&sever_address,sizeof(sever_address)) == -1) 
+    {
+        perror("Failed to bind on socket\n");
+        exit(errno);
+    }
+ 
+    //监听
+    if(ret=listen(socketfd,1) == -1)
+    {
+        perror("Failed to listen on socket\n");
+        exit(errno);
     }
 
-    TCP_connect();
+    //这里可以开始创建线程了
 
+    while(true)
+    {
+        cout << "db db db I am here here here. Connecting...\n";
+        //等待连接
+        //https://blog.csdn.net/daiyudong2020/article/details/51868813
+        struct sockaddr_in client;
+        socklen_t client_addrlength = sizeof(client);
+        connfd = accept(socketfd, (struct sockaddr*)&client, &client_addrlength);   //返回连接套接字描述符
+        cout << "db db db connfd: " << connfd << "\n";
+        if(connfd<0)    //该描述符不为0
+        {
+            perror("err connfd\n");
+        }
+
+        cout << "db db db I am here here here. Connect OKOKOK\n";
+
+        http(connfd);
+
+    }
+	close(socketfd);
 	
     return 0;
 }
---
+++ Lab2/main2.cpp
@@ -0,0 +1,140 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/socket.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<sys/sendfile.h>
+#include<fcntl.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+#include<assert.h>
+#include<unistd.h>
+#include<string.h>
+#include <errno.h>
+#include <iostream>
+
+#include "httprequestparser.h"
+#include "request.h"
+using namespace httpparser;
+
+int server_port;
+
+void handle_request(char text[1024])
+{
+    Request request;
+    HttpRequestParser parser;
+    int length=strlen(text);
+
+    HttpRequestParser::ParseResult res = parser.parse(request, text, text + length);
+
+    if( res == HttpRequestParser::ParsingCompleted )
+    {
+        std::cout<<request.method<<std::endl;
+        std::cout<<request.uri<<std::endl;
+        //method(GET OR POST)
+        //uri(/,/Post_show)
+        //content(e.g. Name=HNU&ID=CS06142)
+    }
+}
+
+void TCP_connect()
+{
+    int socketfd;
+    int connfd;
+    struct sockaddr_in server_address;
+
+    socketfd = socket(AF_INET,SOCK_STREAM,0);   //建立TCP socket
+    if (socketfd == -1) {
+    perror("Failed to create a new socket");
+    exit(errno);
+    }
+	
+	int reuse = 1;
+    if(setsockopt(socketfd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0)
+    {
+      perror("setsockopt error\n");
+      return -1;
+    }
+
+    bzero(&server_address,sizeof(server_address));    //清零
+    server_address.sin_family = AF_INET; 
+    server_address.sin_addr.s_addr = htons(INADDR_ANY);  //sin_addr存储IP地址，使用in_addr这个数据结构；s_addr按照网络字节顺序存储IP地址
+    server_address.sin_port = htons(server_port);   //端口号
+ 
+
+    if (bind(socketfd, (struct sockaddr *) &server_address,
+        sizeof(server_address)) == -1) {
+        perror("Failed to bind on socket");
+        exit(errno);
+    }
+
+    if (listen(socketfd, 1024) == -1) 
+    {
+        perror("Failed to listen on socket");
+        exit(errno);
+    }
+
+    printf("Listening on port %d...\n",server_port);
+
+    while(true)
+    {
+        //等待连接
+        //https://blog.csdn.net/daiyudong2020/article/details/51868813
+        struct sockaddr_in client;
+        socklen_t client_addrlength = sizeof(client);
+        connfd = accept(socketfd, (struct sockaddr*)&client, &client_addrlength);   //返回连接套接字描述符
+        if(connfd<0)    //该描述符不为0
+        {
+            perror("err connfd\n");
+        }
+        else
+        {
+            char request[1024];
+            recv(connfd,request,1024,0);
+            request[strlen(request)+1]='\0';
+
+            handle_request(request);
+
+            printf("successeful!\n");
+            char buf[520]="HTTP/1.1 200 ok\r\nconnection: close\r\n\r\n";//HTTP响应
+            int s = send(connfd,buf,strlen(buf),0);//发送响应
+            //printf("send=%d\n",s);
+            int fd = open("index.html",O_RDONLY);//消息体
+            sendfile(connfd,fd,NULL,2500);//零拷贝发送消息体
+            close(fd);
+            close(connfd);
+        }
+    }
+	close(socketfd);
+
+}
+
+int main(int argc,char **argv)
+{
+    //默认参数s
+    server_port=8888;
+
+    if(argc < 0)
+    {
+        printf("./httpServer --ip ip_address --port port_number [--number-thread thread_number] \n");
+        exit(1);
+    }
+
+    for(int i=1;i<argc;i++)
+    {
+        if(strcmp("--port",argv[i])==0)  // --port
+        {
+            char *server_port_str=argv[++i];
+            if(!server_port_str){
+                fprintf(stderr, "Expected argument after --port\n");
+            }
+            server_port=atoi(server_port_str);
+        }
+        /*else if..*/
+    }
+
+    TCP_connect();
+
+	
+    return 0;
+}
